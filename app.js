4a06ce544656a999e9c878da7cad67d29b1d234f
YainaIvanova <avonavi24@gmail.com>
2021-02-05 17:44:24 -0600

    Responsive Layout Grid

---
+++ app.js
@@ -0,0 +1,7 @@
+// "alert("Hola Mundo"); "
+// var cadena = "ABRIaSLSmLDKDKoDr";
+// var expresion = cadena.toLowerCase();
+// var array_emparejamientos = cadena.match(expresion);
+// console.log(array_emparejamientos);
+var textoMayusculas="ALFABETO"
+console.log(textoMayusculas.toLowerCase()); 
